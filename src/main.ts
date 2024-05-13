62e7b928668bdb7345c5fb0e6203d95e7c12c117
m-Mateja <>
2024-05-13 17:50:49 -0400

    bootstrap to home component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {HomePageComponent} from "./app/components/home-page/home-page.component";
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(HomePageComponent, appConfig)
   .catch((err) => console.error(err));
