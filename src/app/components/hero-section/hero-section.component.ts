8d8b2a233f101a1153d0f605c171a25b5b20e500
m-Mateja <>
2024-05-13 20:15:27 -0400

    hero section started

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [
+    MatGridList,
+    MatGridTile,
+    MatButton
+  ],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css'
 })
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {NavComponent} from "../nav/nav.component";
 import {MatButton} from "@angular/material/button";
+import {HeroSectionComponent} from "../hero-section/hero-section.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, MatButton],
+  imports: [RouterOutlet, NavComponent, MatButton, HeroSectionComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
