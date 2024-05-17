a5a668f6e55e8896cf81659042f831ce3e7055b8
m-Mateja <>
2024-05-17 19:58:50 -0400

    should you hire me counter

--- src/app/components/cool-section/cool-section.component.ts
+++ src/app/components/cool-section/cool-section.component.ts
@@ -7,11 +7,12 @@ import {ClickService} from "../../services/click.service";
 import {CommonModule} from "@angular/common";
 import {TimerService} from "../../services/timer.service";
 import {MouseDistanceService} from "../../services/mouse-distance.service";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-cool-section',
   standalone: true,
-    imports: [MatCardModule, MatExpansionModule, MatTableModule, CommonModule],
+    imports: [MatCardModule, MatExpansionModule, MatTableModule, CommonModule, MatButtonModule],
   templateUrl: './cool-section.component.html',
   styleUrl: './cool-section.component.css'
 })
@@ -22,6 +23,10 @@ export class CoolSectionComponent implements OnInit{
   timerSubscription!: Subscription;
   totalMouseDistance: number = 0;
   mouseDistanceSubscription!: Subscription;
+
+  hireSelection:string = '???'
+  noHireClickCounter:number = 0
+
   constructor(public clickService: ClickService,
               public timerService: TimerService,
               public mouseDistanceService: MouseDistanceService) {
@@ -38,5 +43,26 @@ export class CoolSectionComponent implements OnInit{
     this.mouseDistanceSubscription = this.mouseDistanceService.getDistance().subscribe(distance => {
       this.totalMouseDistance = distance;
     });
+  }
+
+  yesClick(){
+    this.hireSelection = 'Yes'
+    this.noHireClickCounter = 0
+  }
+
+  noClick(){
+    this.noHireClickCounter ++
+    if(this.noHireClickCounter == 1){
+      this.hireSelection = "You missed the Yes button"
+    }
+    else if(this.noHireClickCounter == 2){
+      this.hireSelection = "Wrong one again..."
+    }
+    else if(this.noHireClickCounter == 3){
+      this.hireSelection = ':('
+    }
+    else if(this.noHireClickCounter >3){
+      this.hireSelection = 'Ok, so Yes'
+    }
   }
 }
